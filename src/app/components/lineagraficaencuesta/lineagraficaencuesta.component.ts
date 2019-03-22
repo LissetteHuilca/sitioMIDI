016ba74028c7edb64539cfbcb1badb6c58d7191c
LissetteHuilca <lissette.huilca@gmail.com>
2019-03-22 13:31:13 -0500

    Update lineagraficaencuesta.component.ts

--- src/app/components/lineagraficaencuesta/lineagraficaencuesta.component.ts
+++ src/app/components/lineagraficaencuesta/lineagraficaencuesta.component.ts
@@ -3,6 +3,8 @@ import { LineagraficaencuestaService } from '../../services/lineagraficaencuesta
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Lineagrafica } from 'src/app/models/lineagrafica';
+import { User } from 'src/app/models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-lineagraficaencuesta',
@@ -12,18 +14,25 @@ import { Lineagrafica } from 'src/app/models/lineagrafica';
 })
 export class LineagraficaencuestaComponent implements OnInit {
   private linea: Lineagrafica;
-  
-  constructor(private lineagraficaencuestaService: LineagraficaencuestaService, private router: Router) { }
+  public usserLogged:User;
+
+  constructor(private lineagraficaencuestaService: LineagraficaencuestaService, private router: Router,private userService: UserService) { }
 
   ngOnInit() {
-   
+    this.usserLogged=JSON.parse(localStorage.getItem("currentUser"));
     this.linea = new Lineagrafica();
    
   }
   addLineaGrafica(form: NgForm) {
     this.lineagraficaencuestaService.postLineagrafica(form.value)
       .subscribe(res => {
-     window.open("inicio","_self","")
+        this.usserLogged.estadolineagrafica="si";
+        this.userService.putUser(this.usserLogged)
+        .subscribe(res => {
+          localStorage.setItem('currentUser', JSON.stringify(this.usserLogged));
+        window.open("inicio","_self","")
+        });
+        
      
        
    
